aab0f9d6e1d38d97de4f39df9241e574afc2c43b
norsween <norsween@gmail.com>
2018-07-15 20:32:29 +0100

    implemented user story to scaffold and set-up for deployment my blocipedia-node project

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,20 +4,20 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
-// Set the test suite title to match HTTP verb and route it will test
+  // Set the test suite title to match HTTP verb and route it will test
   describe("GET /", () => {
 
-// Status code of 200 indicates that the request was successful
+    // Status code of 200 indicates that the request was successful
     it("should return status code 200", (done) => {
 
-// Send GET request to the base URL
+      // Send GET request to the base URL
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+	expect(body).toContain("Welcome to Blocipedia-node");
 
-// done method lets Jasmine know test is completed
+        // done method lets Jasmine know test is completed
         done();
       });
     });
-
   });
 });
