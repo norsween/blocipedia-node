1f4f9be1e18bbded44c7f28a2f72776a6b8e1f60
norsween <norsween@gmail.com>
2018-07-15 02:09:14 +0100

    WIP deploy to production

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+// Set the test suite title to match HTTP verb and route it will test
+  describe("GET /", () => {
+
+// Status code of 200 indicates that the request was successful
+    it("should return status code 200", (done) => {
+
+// Send GET request to the base URL
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+// done method lets Jasmine know test is completed
+        done();
+      });
+    });
+
+  });
+});
